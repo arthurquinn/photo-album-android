7e342c1eb11d66754e3147180a10e55d1e7098d7
Alixxa <alixxa.tirisfal@gmail.com>
2016-12-14 09:05:19 -0500

    Added error dialog for missing name in AddEditAlbumActivity

--- app/src/main/java/src/androidphotoalbum/AddEditAlbumActivity.java
+++ app/src/main/java/src/androidphotoalbum/AddEditAlbumActivity.java
@@ -1,9 +1,11 @@
 package src.androidphotoalbum;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -40,19 +42,33 @@ public class AddEditAlbumActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()){
             case R.id.mnuSave:
-                // TODO: Check to make sure text box isn't empty
-                String albumName = txtAlbumName.getText().toString();
-                Intent albumNameIntent = new Intent();
-                albumNameIntent.putExtra("ALBUM_NAME", albumName);
-                setResult(RESULT_OK, albumNameIntent);
-                finish();
+                if (!txtAlbumName.getText().toString().isEmpty()) {
+                    // TODO: Check to make sure text box isn't empty
+                    String albumName = txtAlbumName.getText().toString();
+                    Intent albumNameIntent = new Intent();
+                    albumNameIntent.putExtra("ALBUM_NAME", albumName);
+                    setResult(RESULT_OK, albumNameIntent);
+                    finish();
+                }
+                else {
+                    AlertDialog.Builder missingNameAlert = new AlertDialog.Builder(this);
+                    missingNameAlert.setMessage("Album name field is required")
+                            .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            })
+                            .setTitle("Error")
+                            .create();
+                    missingNameAlert.show();
+                }
                 return true;
             case R.id.mnuCancel:
                 setResult(RESULT_CANCELED);
                 finish();
                 return true;
-            default:
-                return super.onOptionsItemSelected(item);
         }
+        return super.onOptionsItemSelected(item);
     }
 }
